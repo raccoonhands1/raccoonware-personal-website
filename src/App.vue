18048f24360c430cd6a3aae451bc78cd232761b4
raccoonhands1 <reptile12456@gmail.com>
2024-11-27 18:40:10 -0800

    harmless commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
@@ -10,8 +11,10 @@ document.body.appendChild( renderer.domElement );
 
 const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+const cube = new THREE.Mesh( cubeGeometry, cubeMaterial ) as THREE.Mesh & { isRedirectObject?: boolean };
 scene.add( cube );
+
+cube.isRedirectObject = true;
 
 const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 const points = [];
@@ -38,30 +41,41 @@ scene.add( pointLight );
 camera.lookAt( 0, 0, 0 );
 camera.position.z = 10;
 
-let x = 5;
-let y = 2;
+let x = 0;
+let y = 0;
+
+let myMap = new Map<
 
 window.addEventListener( 'pointermove', onPointerMove );
+window.addEventListener( 'click', onClick );
+
+function onClick( event ) {
+
+	raycaster.setFromCamera( pointer, camera );
+
+	const intersects = raycaster.intersectObjects( scene.children );
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+		intersects[i].object.material.color.set( 0xff0000 );
+		if(intersects[i].object?.isRedirectObject){
+			setTimeout(() => {
+				window.open("https://www.youtube.com/watch?v=wdh2axFIXc0&pp=ygUJI2dheXNraXNz", "_blank");
+			}, 6000);
+		}
+	}
+}
 
 function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	cube.rotation.z += 0.02;
 	
-	camera.position.x = 5 * Math.sin(x);
-	x+=0.01;
-	camera.position.y = 3 * Math.sin(y);
-	y-=0.03;
+	camera.position.x = 1 * Math.sin(x);
+	x+=0.003;
+	camera.position.y = 1 * Math.sin(y);
+	y-=0.005;
 
-	// update the picking ray with the camera and pointer position
-	raycaster.setFromCamera( pointer, camera );
-
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children );
-
-	for ( let i = 0; i < intersects.length; i ++ ) {
-		intersects[ i ].object.material.color.set( 0xff0000 );
-	}
+	
 
 	renderer.render( scene, camera );
 }
